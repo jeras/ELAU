388265a132aba11c6151b47c8fad56a6235a2b48
Iztok Jeras <iztok.jeras@gmail.com>
2026-08-05 19:08:16 +0200

    test: small generalization

--- test/test.py
+++ test/test.py
@@ -101,116 +101,8 @@ sources = [f"../src/{unit}.sv" for unit in [
     "SumZeroDet",
 ]]
 
-for dut in [
-    # Adders
-    DutUnit("Add"                  , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-    DutUnit("AddC"                 , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-    DutUnit("AddCfast"             , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-    DutUnit("AddV"                 , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-    DutUnit("AddMod2Nm1"           , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-    DutUnit("AddMod2Nm1s0"         , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-#   DutUnit("AddMod2Np1"           , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # equivalence fail
-    DutUnit("AddCsv"               , [                            Timing.FAST]),
-    DutUnit("AddMop"               , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-#    DutUnit("AddMopCsv"            , [Timing.SLOW,                Timing.FAST]), # missing behavioral
 
-    # helper for AddMod2N*
-#   DutUnit("PrefixAndOrCendaround", [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # missing behavioral
-    # helper for AddCfast
-#   DutUnit("PrefixAndOrCfast"     , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # missing behavioral
-
-    # Subtractors, Complementers
-    DutUnit("Sub"                  , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-    DutUnit("SubC"                 , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-#   DutUnit("SubCZ"                , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # proof failed
-    DutUnit("SubV"                 , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-#   DutUnit("SubVZ"                , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # proof failed
-    DutUnit("Neg"                  , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-    DutUnit("NegC"                 , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-    DutUnit("AbsVal"               , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-
-    # Adder-Subtractors
-    DutUnit("AddSub"               , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-    DutUnit("AddSubC"              , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-    DutUnit("AddSubV"              , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-
-    # Incrementers, Decrementers
-    DutUnit("Inc"                  , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-    DutUnit("IncC"                 , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-    DutUnit("Dec"                  , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-    DutUnit("DecC"                 , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-    DutUnit("IncDec"               , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-    DutUnit("IncDecC"              , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-
-    # Comparators
-    DutUnit("CmpEQ"                , [                            Timing.FAST]),
-    DutUnit("CmpGE"                , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-    DutUnit("CmpEQGE"              , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-
-    # Multipliers
-#   DutUnit("MulSgn"               , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # proof takes a long time
-#   DutUnit("MulUns"               , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # equivalence timeout
-#   DutUnit("MulAddSgn"            , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-#   DutUnit("MulAddUns"            , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-#   DutUnit("AddMulSgn"            , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # equivalence timeout
-#   DutUnit("AddMulUns"            , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # equivalence timeout
-#   DutUnit("MulCsvSgn"            , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-#   DutUnit("MulCsvUns"            , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-#   DutUnit("SqrSgn"               , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # ERROR: No SAT model available for cell $flatten\gate.$0 ($pow).
-#   DutUnit("SqrUns"               , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # ERROR: No SAT model available for cell $flatten\gate.$0 ($pow).
-
-    # helpers for Sqr*
-#   DutUnit("SqrPPGenSgn"          , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # missing behavioral
-#   DutUnit("SqrPPGenUns"          , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # missing behavioral
-
-    # helpers
-#   DutUnit("MulPPGenSgn"          , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # missing behavioral
-#   DutUnit("MulPPGenUns"          , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # missing behavioral
-#   DutUnit("AddMulPPGenSgn"       , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # missing behavioral
-#   DutUnit("AddMulPPGenUns"       , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # missing behavioral
-
-    # Dividers, Square-Root Extractors
-#   DutUnit("DivArrSgn"            , [Timing.SLOW                            ]), # equivalence fail
-#   DutUnit("DivArrUns"            , [Timing.SLOW                            ]), # equivalence fail
-#   DutUnit("SqrtArrUns"           , [Timing.SLOW                            ]), # ERROR: Feature unimplemented at /home/izi/VLSI/yosys/frontends/slang/lib/src/slang_frontend.cc:799, see AST and code line dump above
-
-    # Detectors
-    DutUnit("AllOneDet"            , [                            Timing.FAST]),
-    DutUnit("AllZeroDet"           , [                            Timing.FAST]),
-    DutUnit("SumZeroDet"           , [                            Timing.FAST]),
-    DutUnit("LeadZeroDet"          , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-    DutUnit("LeadOneDet"           , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-    DutUnit("LeadSignDet"          , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-    DutUnit("Log2"                 , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-
-    # Encoders, Decoders, Gray
-    DutUnit("Decode"               , [                            Timing.FAST]),
-#   DutUnit("Encode"               , [                            Timing.FAST]), # equivalence fail
-    DutUnit("Bin2Gray"             , [                            Timing.FAST]),
-    DutUnit("Gray2Bin"             , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-    DutUnit("IncGray"              , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-    DutUnit("IncGrayC"             , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-
-    # Miscellaneous
-    DutUnit("Cnt"                  , [Timing.SLOW,                Timing.FAST]),
-#   DutUnit("Cpr"                  , [Timing.SLOW,                Timing.FAST]), # missing behavioral
-    DutUnit("RedAnd"               , [                            Timing.FAST]),
-    DutUnit("RedOr"                , [                            Timing.FAST]),
-    DutUnit("RedXor"               , [                            Timing.FAST]),
-    DutUnit("PrefixAnd"            , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # missing behavioral
-    DutUnit("PrefixOr"             , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # missing behavioral
-#   DutUnit("PrefixAndOr"          , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # missing behavioral
-    DutUnit("PrefixXor"            , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # missing behavioral
-
-    # helper for Cnt
-#   DutUnit("CntSlice"             , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # missing behavioral
-
-    # helper for many
-    DutUnit("FullAdder"            , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
-
-    # helper unused
-#   DutUnit("Reg"                  , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # missing behavioral
-]:
+def unit_test(dut: DutUnit):
 
     for timing in dut.timing:
 
@@ -330,3 +222,117 @@ for dut in [
 #        subprocess.Popen('netlistsvg {dut.name}_netlist.json -o {dut.name}_netlist.svg', shell=True)
 
         ys.run_pass(f"design -reset")
+
+units = [
+    # Adders
+    DutUnit("Add"                  , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+    DutUnit("AddC"                 , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+    DutUnit("AddCfast"             , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+    DutUnit("AddV"                 , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+    DutUnit("AddMod2Nm1"           , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+    DutUnit("AddMod2Nm1s0"         , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+#   DutUnit("AddMod2Np1"           , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # equivalence fail
+    DutUnit("AddCsv"               , [                            Timing.FAST]),
+    DutUnit("AddMop"               , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+#    DutUnit("AddMopCsv"            , [Timing.SLOW,                Timing.FAST]), # missing behavioral
+
+    # helper for AddMod2N*
+#   DutUnit("PrefixAndOrCendaround", [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # missing behavioral
+    # helper for AddCfast
+#   DutUnit("PrefixAndOrCfast"     , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # missing behavioral
+
+    # Subtractors, Complementers
+    DutUnit("Sub"                  , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+    DutUnit("SubC"                 , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+#   DutUnit("SubCZ"                , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # proof failed
+    DutUnit("SubV"                 , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+#   DutUnit("SubVZ"                , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # proof failed
+    DutUnit("Neg"                  , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+    DutUnit("NegC"                 , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+    DutUnit("AbsVal"               , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+
+    # Adder-Subtractors
+    DutUnit("AddSub"               , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+    DutUnit("AddSubC"              , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+    DutUnit("AddSubV"              , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+
+    # Incrementers, Decrementers
+    DutUnit("Inc"                  , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+    DutUnit("IncC"                 , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+    DutUnit("Dec"                  , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+    DutUnit("DecC"                 , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+    DutUnit("IncDec"               , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+    DutUnit("IncDecC"              , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+
+    # Comparators
+    DutUnit("CmpEQ"                , [                            Timing.FAST]),
+    DutUnit("CmpGE"                , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+    DutUnit("CmpEQGE"              , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+
+    # Multipliers
+#   DutUnit("MulSgn"               , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # proof takes a long time
+#   DutUnit("MulUns"               , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # equivalence timeout
+#   DutUnit("MulAddSgn"            , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+#   DutUnit("MulAddUns"            , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+#   DutUnit("AddMulSgn"            , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # equivalence timeout
+#   DutUnit("AddMulUns"            , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # equivalence timeout
+#   DutUnit("MulCsvSgn"            , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+#   DutUnit("MulCsvUns"            , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+#   DutUnit("SqrSgn"               , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # ERROR: No SAT model available for cell $flatten\gate.$0 ($pow).
+#   DutUnit("SqrUns"               , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # ERROR: No SAT model available for cell $flatten\gate.$0 ($pow).
+
+    # helpers for Sqr*
+#   DutUnit("SqrPPGenSgn"          , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # missing behavioral
+#   DutUnit("SqrPPGenUns"          , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # missing behavioral
+
+    # helpers
+#   DutUnit("MulPPGenSgn"          , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # missing behavioral
+#   DutUnit("MulPPGenUns"          , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # missing behavioral
+#   DutUnit("AddMulPPGenSgn"       , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # missing behavioral
+#   DutUnit("AddMulPPGenUns"       , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # missing behavioral
+
+    # Dividers, Square-Root Extractors
+#   DutUnit("DivArrSgn"            , [Timing.SLOW                            ]), # equivalence fail
+#   DutUnit("DivArrUns"            , [Timing.SLOW                            ]), # equivalence fail
+#   DutUnit("SqrtArrUns"           , [Timing.SLOW                            ]), # ERROR: Feature unimplemented at /home/izi/VLSI/yosys/frontends/slang/lib/src/slang_frontend.cc:799, see AST and code line dump above
+
+    # Detectors
+    DutUnit("AllOneDet"            , [                            Timing.FAST]),
+    DutUnit("AllZeroDet"           , [                            Timing.FAST]),
+    DutUnit("SumZeroDet"           , [                            Timing.FAST]),
+    DutUnit("LeadZeroDet"          , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+    DutUnit("LeadOneDet"           , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+    DutUnit("LeadSignDet"          , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+    DutUnit("Log2"                 , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+
+    # Encoders, Decoders, Gray
+    DutUnit("Decode"               , [                            Timing.FAST]),
+    DutUnit("Encode"               , [                            Timing.FAST]), # equivalence fail
+    DutUnit("Bin2Gray"             , [                            Timing.FAST]),
+    DutUnit("Gray2Bin"             , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+    DutUnit("IncGray"              , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+    DutUnit("IncGrayC"             , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+
+    # Miscellaneous
+    DutUnit("Cnt"                  , [Timing.SLOW,                Timing.FAST]),
+#   DutUnit("Cpr"                  , [Timing.SLOW,                Timing.FAST]), # missing behavioral
+    DutUnit("RedAnd"               , [                            Timing.FAST]),
+    DutUnit("RedOr"                , [                            Timing.FAST]),
+    DutUnit("RedXor"               , [                            Timing.FAST]),
+    DutUnit("PrefixAnd"            , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # missing behavioral
+    DutUnit("PrefixOr"             , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # missing behavioral
+#   DutUnit("PrefixAndOr"          , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # missing behavioral
+    DutUnit("PrefixXor"            , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # missing behavioral
+
+    # helper for Cnt
+#   DutUnit("CntSlice"             , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # missing behavioral
+
+    # helper for many
+    DutUnit("FullAdder"            , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]),
+
+    # helper unused
+#   DutUnit("Reg"                  , [Timing.SLOW, Timing.MEDIUM, Timing.FAST]), # missing behavioral
+]
+
+for unit in units:
+    unit_test(unit)
